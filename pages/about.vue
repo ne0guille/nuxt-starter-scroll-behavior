36646c1fc954201ddd32201529f59da5c3764ebc
ne0guille <eyherabideg@gmail.com>
2023-10-24 13:47:18 -0300

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <h1>About page</h1>
+    <section class="green">test</section>
+    <NuxtLink to="/">HOME</NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+};
+</script>
+<style>
+.green {
+  height: 2000px;
+  background-color: green;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+    <h1>Home page</h1>
+    <section class="red">test</section>
+    <NuxtLink to="/about"> About </NuxtLink>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+};
+</script>
+
+<style>
+.red {
+  height: 2000px;
+  background-color: red;
+}
+</style>
